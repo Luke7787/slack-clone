4b5b297b1340b2657442922d922cc987e3d505d9
Luke Zhuang <Lukezhuang@yahoo.com>
2025-10-14 17:01:50 -0700

    refactor code to grab values from env.js rather than .env to be more organized and clean

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,12 @@
-import express from "express"; 
-import dotenv from "dotenv";
-
-dotenv.config();
+import express from "express";
+import { ENV } from "./config/env.js";
 
 const app = express();
-
-const PORT = process.env.PORT;
  
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(PORT, () => console.log("Server started on port:", PORT));
+console.log("mongo uri:", ENV.MONGO_URI);
+
+app.listen(ENV.PORT, () => console.log("Server started on port:", ENV.PORT));
