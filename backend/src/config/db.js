613f00a166a9a173c5065ab3166e6c5272b1b268
Luke Zhuang <Lukezhuang@yahoo.com>
2025-10-16 17:46:36 -0700

    worked on db.js, adding mongoDB connection function using Mongoose

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,1 +1,14 @@
-testing
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
+export const connectDB = async () => {
+
+    try {
+      const conn = await mongoose.connect(ENV.MONGO_URI);
+      console.log("MongoDB connected successfully", conn.connection.host);
+        
+    } catch (error) {
+      console.log("Error connecting to MongoDB:", error);
+      process.exit(1); // status code 1 indicates error, code 0 indicates success
+    }
+};
