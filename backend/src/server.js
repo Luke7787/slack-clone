d8741f5ba183e7ddcb34bb637b798e5e7cf2daa8
Luke Zhuang <Lukezhuang@yahoo.com>
2025-10-18 15:44:32 -0700

    added clerk publishable key and secret key to env.js for authentication

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,8 +8,6 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-console.log("mongo uri:", ENV.MONGO_URI);
-
 app.listen(ENV.PORT, () => {
     console.log("Server started on port:", ENV.PORT)
     connectDB()
