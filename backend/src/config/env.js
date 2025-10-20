ca89dfd3ff8e8057af48072b4ebd3dfa5828769e
Luke Zhuang <Lukezhuang@yahoo.com>
2025-10-19 18:56:33 -0700

    updated env.js with several new API keys

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -6,4 +6,7 @@ export const ENV = {
     NODE_ENV: process.env.NODE_ENV,
     CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    STREAM_API_KEY: process.env.STREAM_API_KEY,
+    STREAM_API_SECRET: process.env.STREAM_API_SECRET,
+    SENTRY_DSN: process.end.SENTRY_DSN,
 }
